b25e71e0ef82d242c870800895851fe5afcaeefd
balinux <ryojuniyantara@gmail.com>
2017-04-07 15:58:22 +0800

    update cara kedua

--- db.js
+++ db.js
@@ -2,31 +2,61 @@ var express = require('express');
 var mysql = require('mysql');
 var app = express();
 
-var connect = mysql.createConnection({
+
+// Cara Pertama
+// var connect = mysql.createConnection({
+//   host:'localhost',
+//   user:'root',
+//   password:'',
+//   database:'networklist'
+// });
+
+// connect.connect(function (error) {
+//   if (!!error) {
+//     console.log('error');
+//     }
+//     else {
+//       console.log('Connected!');
+//     }
+// });
+
+// app.get('/', function (req,resp) {
+//   connect.query("SELECT * FROM users", function (error,rows,fields) {
+//     if (!!error) {
+//       console.log('perintah query error');
+//     }else {
+//       console.log('qery Sukses!!');
+//       console.log(rows[0].name);
+//       resp.send('Hallo, '+rows[0].name);
+//       // console.log(fields);
+//     }
+//   });
+// })
+
+// CARA KEDUA
+var connect = mysql.createPool({
+  limit:100,
   host:'localhost',
   user:'root',
   password:'',
   database:'networklist'
 });
 
-connect.connect(function (error) {
-  if (!!error) {
-    console.log('error');
-    }
-    else {
-      console.log('Connected!');
-    }
-});
-
 app.get('/', function (req,resp) {
-  connect.query("SELECT * FROM users", function (error,rows,fields) {
+connect.getConnection(function (error,tempCont) {
     if (!!error) {
-      console.log('perintah query error');
+      tempCont.release();
+      console.log('Error');
     }else {
-      console.log('qery Sukses!!');
-      console.log(rows[0].name);
-      resp.send('Hallo, '+rows[0].name);
-      // console.log(fields);
+      console.log('Connected!');
+      tempCont.query("SELECT * FROM users",function (error, rows,fields) {
+        if (!!error) {
+            console.log('Koneksi Error');
+        }else {
+          console.log('Koneksi Sukses');
+          resp.json(rows);
+        }
+      })
     }
   });
 })
